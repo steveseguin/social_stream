ca965a61870eac5dcf1fdf87b63295ea835b4f42
Steve Seguin <steve@seguin.email>
2021-09-29 14:57:20 -0400

    Add files via upload

---
+++ settings/options.js
@@ -0,0 +1,35 @@
+function saveOptions(e) {
+  e.preventDefault();
+  chrome.storage.sync.set({
+	streamID: document.querySelector("#streamID").value,
+  });
+  document.querySelector("#savedButton").innerHTML = "Saved";
+}
+function generateStreamID(){
+	var text = "";
+	var possible = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz23456789";
+	for (var i = 0; i < 10; i++){
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	chrome.storage.sync.set({
+		streamID: text
+	});
+	return text;
+};
+function restoreOptions() {
+  var properties = ["streamID"];
+  chrome.storage.sync.get(properties, function(result){
+	try{
+		document.querySelector("#streamID").value = result.streamID || generateStreamID();
+	} catch(e){console.error(e);}
+  });
+
+}
+
+function editedOptions(){
+	document.querySelector("#savedButton").innerHTML = "Save";
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
+document.querySelector("form").addEventListener("input", editedOptions);
