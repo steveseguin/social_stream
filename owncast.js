931176da0328450d206177aacce64025ed9d1a6d
STeve Seguin <steve@seguin.email>
2021-12-07 02:01:00 -0500

    added owncast as an option

---
+++ owncast.js
@@ -0,0 +1,89 @@
+function toDataURL(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function() {
+    var reader = new FileReader();
+    reader.onloadend = function() {
+      callback(reader.result);
+    }
+    reader.readAsDataURL(xhr.response);
+  };
+  xhr.open('GET', url);
+  xhr.responseType = 'blob';
+  xhr.send();
+}
+
+function processMessage(ele, wss=true){
+
+  if(ele.hasAttribute("is-deleted")) {
+    return;
+  }
+  
+  var chatname = "";
+  try{
+	chatname = ele.querySelector(".message-author").innerText;
+  } catch(e){}
+  
+  var chatmessage = "";
+  try{
+	chatmessage = ele.querySelector(".message-text").innerHTML;
+  } catch(e){}
+  
+  var chatimg = "";
+ 
+  var chatdonation = "";
+  var chatmembership = "";
+  var chatsticker = "";
+  var chatbadges = "";
+  var hasDonation = '';
+  var hasMembership = '';
+
+  var data = {};
+  data.chatname = chatname;
+  data.chatbadges = chatbadges;
+  data.chatmessage = chatmessage;
+  data.chatimg = ""; // Doesn't seem to be an avatar image for owncast
+  data.hasDonation = hasDonation;
+  data.hasMembership = hasMembership;
+  data.type = "owncast";
+  
+  try {
+	chrome.runtime.sendMessage(chrome.runtime.id, { "message": data }, function(){});
+  } catch(e){}
+}
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+		try{
+			if ("focusChat" == request){
+				document.querySelector("div#message-input").focus();
+				//document.querySelector("yt-live-chat-text-input-field-renderer").focus();
+			}
+		} catch(e){}
+		sendResponse(document.querySelector("div#message-input").innerHTML);
+	}
+);
+
+function onElementInserted(containerSelector, callback) {
+    var onMutationsObserved = function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.addedNodes.length) {
+				console.log(mutation.addedNodes);
+                for (var i = 0, len = mutation.addedNodes.length; i < len; i++) {
+                    if (mutation.addedNodes[i] && mutation.addedNodes[i].className && mutation.addedNodes[i].classList.contains("message")) {
+                        callback(mutation.addedNodes[i]);
+                    }
+                }
+            }
+        });
+    };
+    var target = document.querySelector(containerSelector);
+	if (!target){return;}
+    var config = { childList: true, subtree: true };
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    var observer = new MutationObserver(onMutationsObserved);
+    observer.observe(target, config);
+}
+console.log("starting");
+onElementInserted("#messages-only", function(element){
+  processMessage(element, false);
+});
