57b4b58e4da27818c6243bfb6c36d3914eb5ff8e
steveseguin <punkrawker@gmail.com>
2022-12-21 18:51:22 -0500

    random color option added for names

--- background.js
+++ background.js
@@ -349,6 +349,38 @@ function YouTubeGetID(url){
   }
   return ID;
 }
+
+var colours = 167772;
+function rainbow(step) {
+	var r, g, b;
+	var h = 1 - (step / colours);
+	var i = ~~(h * 6);
+	var f = h * 6 - i;
+	var q = 1 - f;
+	switch(i % 6){
+		case 0: r = 1, g = f, b = 0; break;
+		case 1: r = q, g = 1, b = 0; break;
+		case 2: r = 0, g = 1, b = f; break;
+		case 3: r = 0, g = q, b = 1; break;
+		case 4: r = f, g = 0, b = 1; break;
+		case 5: r = 1, g = 0, b = q; break;
+	}
+	var c = "#" + ("00" + (~ ~(r * 200+35)).toString(16)).slice(-2) + ("00" + (~ ~(g * 200 +35)).toString(16)).slice(-2) + ("00" + (~ ~(b * 200 +35)).toString(16)).slice(-2);
+	return (c);
+}
+
+function getColorFromName(str) {
+  var out = 0, len = str.length;
+  if (len>6){
+	  len = 6;
+  }
+  for (pos = 0; pos < len; pos++) {
+    out += (str.charCodeAt(pos) - 64) * Math.pow(26, len - pos - 1);
+  }
+  out = out%colours; // get modulus
+  out = rainbow(out)
+  return out;
+}
 		
 chrome.runtime.onMessage.addListener(
     async function (request, sender, sendResponse) {
@@ -769,6 +801,10 @@ function sendToDestinations(message){
 	if (typeof message == "object"){
 		messageCounter+=1;
 		message.id = messageCounter;
+	}
+	console.log(message);
+	if (settings.randomcolor && message && ("nameColor" in message) && !message.nameColor && message.chatname){
+		message.nameColor = getColorFromName(message.chatname);
 	}
 	
 	sendDataP2P(message);
