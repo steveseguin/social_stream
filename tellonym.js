c85ddf8158486a3c60fa269a72184403381a68d2
steveseguin <punkrawker@gmail.com>
2023-03-18 21:24:41 -0400

    tellonym.me support added

--- tellonym.js
+++ tellonym.js
@@ -14,64 +14,23 @@
 	  xhr.send();
 	}
 	
-	var lastMessage = "";
-	var lastName = "";
-	var timer = null;
 	
 	function processMessage(ele){
-		var name="";
-		try {
-			name = ele.dataset.username || "";
-			
-		} catch(e){
-			name = "";
-		}
 		
-		if (!name){
-			name = ele.childNodes[0].childNodes[1].innerText;
-		}
+		var msg = ele.textContent;
 		
-		var msg = "";
-		try {
-			ele.childNodes[1].childNodes.forEach(ee=>{
-				if (ee.nodeType == Node.TEXT_NODE){
-					msg += ee.textContent;
-				} else if (settings.textonlymode && ee.alt && (ee.nodeName  == "IMG")){
-				//	msg += ee.alt;
-				} else if (!settings.textonlymode&& (ee.nodeName  == "IMG")){
-					msg += "<img src='"+ee.src+"' />";
-				}  else {
-					msg += ee.textContent;
-				}
-			});
-		}catch(e){msg = "";}
-		
-		msg = msg.trim();
-		
-		var chatimg = '';
 		
 		var data = {};
-		data.chatname = name;
+		data.chatname = "";
 		data.chatbadges = "";
 		data.backgroundColor = "";
 		data.textColor = "";
 		data.chatmessage = msg;
-		data.chatimg = chatimg;
+		data.chatimg = "";
 		data.hasDonation = "";
 		data.hasMembership = "";;
 		data.contentimg = "";
-		data.type = "wix";
-		
-		if ((lastMessage == msg) && (lastName == name)){
-			return;
-		}
-		lastMessage = msg;
-		lastName = name;
-		clearTimeout(timer);
-		timer = setTimeout(function(){
-			lastMessage = null;
-			lastName = null;
-		},2000);
+		data.type = "tellonym";
 		
 		if (!msg){return;}
 		
@@ -101,13 +60,6 @@
 	chrome.runtime.onMessage.addListener(
 		function (request, sender, sendResponse) {
 			try{
-				if ("focusChat" == request){ // if (prev.querySelector('[id^="message-username-"]')){ //slateTextArea-
-					if (window.location.pathname.includes("live-video")){
-						document.querySelector('textarea').focus();
-						sendResponse(true);
-						return;
-					}
-				}
 				if (typeof request === "object"){
 					if ("settings" in request){
 						settings = request.settings;
@@ -127,10 +79,11 @@
 				for (var i=0;i<nodes.length;i++){
 					try {
 						var ele = nodes[i];
-						if (ele && ele.parentNode && (ele.parentNode.dataset.hook=="MESSAGES_CONTAINER")){
-							if (!ele.skip){
-								ele.skip = true;
-								processMessage(ele);
+						if (ele){
+							var ttt = ele.querySelector("div>div>div>div>span");
+							if (ttt && !ttt.skip){
+								ttt.skip = true;
+								processMessage(ttt);
 							}
 						}
 					}catch(e){console.error(e)}
@@ -147,21 +100,18 @@
 	console.log("social stream injected");
 
 	setInterval(function(){
-		if (window.location.pathname.includes("live-video")){
-			var chatContainer = document.querySelector('[data-hook="MESSAGES_CONTAINER"]')
-			if (chatContainer){
-				if (!chatContainer.marked){
-					console.log("social stream activated");
-					chatContainer.marked=true;
-					chatContainer.childNodes.forEach(ele=>{
-						if (ele && ele.nodeName && ele.nodeName == "DIV"){
-							ele.skip = true;
-						}
-					});
-					onElementInserted(chatContainer);
-				}
+		var chatContainer = document.querySelector('#root')
+		if (chatContainer){
+			if (!chatContainer.marked){
+				console.log("social stream activated");
+				chatContainer.marked=true;
+				chatContainer.querySelectorAll("div>div>div>div>span").forEach(ele=>{
+					ele.skip = true;
+				//	processMessage(ele); // we don't want to process existing messages.
+				});
+				onElementInserted(chatContainer);
 			}
-		}
+			}
 	},1000);
 
 })();
