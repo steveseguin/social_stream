1146fbca3c7ced3c049a9155543f8918ee601b3a
steveseguin <punkrawker@gmail.com>
2023-04-08 09:31:34 -0400

    joystick.tv added; no dono support yet

---
+++ joystick.js
@@ -0,0 +1,247 @@
+(function () {
+	
+	
+	async function fetchWithTimeout(URL, timeout=8000){ // ref: https://dmitripavlutin.com/timeout-fetch-request/
+		try {
+			const controller = new AbortController();
+			const timeout_id = setTimeout(() => controller.abort(), timeout);
+			const response = await fetch(URL, {...{timeout:timeout}, signal: controller.signal});
+			clearTimeout(timeout_id);
+			return response;
+		} catch(e){
+			errorlog(e);
+			return await fetch(URL); // iOS 11.x/12.0
+		}
+	}
+
+
+	function getAllContentNodes(element) {
+		var resp = "";
+		element.childNodes.forEach(node=>{
+			if (node.childNodes.length){
+				resp += getAllContentNodes(node)
+			} else if ((node.nodeType === 3) && (node.textContent.trim().length > 0)){
+				if (settings.textonlymode){
+					resp += node.textContent.trim()+" ";
+				} else {
+					resp += node.textContent.trim()+" ";
+				}
+			} else if (node.nodeType === 1){
+				if (settings.textonlymode){
+				//	if ("alt" in node){
+				//		resp += node.alt.trim()+" ";
+					//}
+				} else {
+					resp += node.outerHTML;
+				}
+			} 
+		});
+		return resp;
+	}
+	
+	var lastMessage = "";
+	var lastUser  = "";
+	
+	function processMessage(ele, bot=false){	// twitch
+	
+	  var chatsticker = false;
+	  var chatmessage = "";
+	  var nameColor = "";
+	  
+	  try {
+		var chatname = ele.querySelector(".username").innerText;
+	  } catch(e){
+		 return;
+	  }
+	  
+	  chatname = chatname.trim();
+	  if (!chatname){return;}
+	  
+	  try {
+		  nameColor = ele.querySelector(".username>span").style.color;
+	  } catch(e){
+	  }
+	  
+	  var chatbadges = [];
+	  var hasDonation = '';
+	  
+	  
+	  if (ele.querySelector("[title='Streamer']")){ // host.
+		  var badge = {};
+		  badge.html = '<?xml version="1.0" encoding="utf-8"?><svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M19.965 8.521C19.988 8.347 20 8.173 20 8c0-2.379-2.143-4.288-4.521-3.965C14.786 2.802 13.466 2 12 2s-2.786.802-3.479 2.035C6.138 3.712 4 5.621 4 8c0 .173.012.347.035.521C2.802 9.215 2 10.535 2 12s.802 2.785 2.035 3.479A3.976 3.976 0 0 0 4 16c0 2.379 2.138 4.283 4.521 3.965C9.214 21.198 10.534 22 12 22s2.786-.802 3.479-2.035C17.857 20.283 20 18.379 20 16c0-.173-.012-.347-.035-.521C21.198 14.785 22 13.465 22 12s-.802-2.785-2.035-3.479zm-9.01 7.895-3.667-3.714 1.424-1.404 2.257 2.286 4.327-4.294 1.408 1.42-5.749 5.706z"/></svg>';
+		  badge.type = "svg";
+		  chatbadges.push(badge);
+	  }
+	  
+	  // need to add dono support
+	  
+	  try {
+		var eleContent = ele.querySelector(".content > span:nth-of-type(2):not([class])");
+		chatmessage = getAllContentNodes(eleContent);
+	  } catch(e){
+		  return;
+	  }
+	  
+	 if (chatmessage){
+		 chatmessage = chatmessage.trim();
+	 }
+	 
+	 
+	 if ((lastMessage === chatmessage) && (lastUser === chatname)){
+		  lastMessage = "";
+		  chatname = "";
+		  return;
+	  } else {
+		lastMessage = chatmessage;
+		lastUser = chatname;
+	  }
+	  
+	  
+	  var chatimg = "";
+	
+	 
+	  if (!chatmessage && !hasDonation){
+		return;
+	  }
+
+	  var data = {};
+	  data.chatname = chatname;
+	  data.chatbadges = chatbadges;
+	  data.nameColor = nameColor;
+	  data.chatmessage = chatmessage;
+	  data.chatimg = chatimg;
+	  data.hasDonation = hasDonation;
+	  data.hasMembership = "";
+	  data.type = "joystick";
+	  if (bot){
+		data.bot = bot;
+	  }
+	//  console.log(data);
+	  
+	 
+	  try {
+		chrome.runtime.sendMessage(chrome.runtime.id, { "message": data }, function(e){});
+	  } catch(e){
+		  //
+	  }
+	}
+
+	chrome.runtime.onMessage.addListener(
+		function (request, sender, sendResponse) {
+			try{
+				if ("focusChat" == request){
+					document.querySelector('input[flow-id="chat-message-text-input"]').focus();
+					sendResponse(true);
+					return;
+				}
+				if (typeof request === "object"){
+					if ("settings" in request){
+						settings = request.settings;
+						sendResponse(true);
+						return;
+					}
+				}
+				// twitch doesn't capture avatars already.
+			} catch(e){}
+			sendResponse(false);
+		}
+	);
+	
+	var settings = {};
+	// settings.textonlymode
+	// settings.streamevents
+	
+	
+	chrome.runtime.sendMessage(chrome.runtime.id, { "getSettings": true }, function(response){  // {"state":isExtensionOn,"streamID":channel, "settings":settings}
+		if ("settings" in response){
+			settings = response.settings;
+		}
+	});
+
+	function onElementInserted(target) {
+		var onMutationsObserved = function(mutations) {
+			
+			mutations.forEach(function(mutation) {
+				if (mutation.addedNodes.length) {
+					for (var i = 0, len = mutation.addedNodes.length; i < len; i++) {
+						try {
+							if (mutation.addedNodes[i].nodeName.toLowerCase() !== "div"){continue;}
+							if (mutation.addedNodes[i].ignore){continue;}
+							mutation.addedNodes[i].ignore=true;
+							if (mutation.addedNodes[i].classList.contains("chat-message")){
+								if (!mutation.addedNodes[i].classList.contains("event_bot_message")){
+									processMessage(mutation.addedNodes[i]);
+								} else {
+									processMessage(mutation.addedNodes[i], true);
+								}
+							}
+						} catch(e){}
+					}
+				}
+			});
+		};
+		
+		var config = { childList: true, subtree: true };
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		var observer = new MutationObserver(onMutationsObserved);
+		observer.observe(target, config);
+	}
+	
+	console.log("Social Stream injected");
+	
+	var checkReady = setInterval(function(){
+		var mainChat = document.querySelector("#chat-messages");
+		if (mainChat){ // just in case 
+			console.log("Social Stream Start");
+			clearInterval(checkReady);
+			
+			setTimeout(function(){
+				var clear = mainChat.querySelectorAll(".chat-message");
+				for (var i = 0;i<clear.length;i++){
+					clear[i].ignore = true; // don't let already loaded messages to re-load.
+					//processMessage(clear[i]); // testing only.
+				}
+				console.log("Social Stream ready to go");
+				onElementInserted(mainChat);
+			},1000);
+		} 
+	},500);
+	
+	///////// the following is a loopback webrtc trick to get chrome to not throttle this twitch tab when not visible.
+	try {
+		var receiveChannelCallback = function(e){
+			remoteConnection.datachannel = event.channel;
+			remoteConnection.datachannel.onmessage = function(e){};;
+			remoteConnection.datachannel.onopen = function(e){};;
+			remoteConnection.datachannel.onclose = function(e){};;
+			setInterval(function(){
+				if (document.hidden){ // only poke ourselves if tab is hidden, to reduce cpu a tiny bit.
+					remoteConnection.datachannel.send("KEEPALIVE")
+				}
+			}, 800);
+		}
+		var errorHandle = function(e){}
+		var localConnection = new RTCPeerConnection();
+		var remoteConnection = new RTCPeerConnection();
+		localConnection.onicecandidate = (e) => !e.candidate ||	remoteConnection.addIceCandidate(e.candidate).catch(errorHandle);
+		remoteConnection.onicecandidate = (e) => !e.candidate || localConnection.addIceCandidate(e.candidate).catch(errorHandle);
+		remoteConnection.ondatachannel = receiveChannelCallback;
+		localConnection.sendChannel = localConnection.createDataChannel("sendChannel");
+		localConnection.sendChannel.onopen = function(e){localConnection.sendChannel.send("CONNECTED");};
+		localConnection.sendChannel.onclose =  function(e){};
+		localConnection.sendChannel.onmessage = function(e){};
+		localConnection.createOffer()
+			.then((offer) => localConnection.setLocalDescription(offer))
+			.then(() => remoteConnection.setRemoteDescription(localConnection.localDescription))
+			.then(() => remoteConnection.createAnswer())
+			.then((answer) => remoteConnection.setLocalDescription(answer))
+			.then(() =>	{
+				localConnection.setRemoteDescription(remoteConnection.localDescription);
+				console.log("KEEP ALIVE TRICk ENABLED");
+			})
+			.catch(errorHandle);
+	} catch(e){
+		console.log(e);
+	}
+
+})();
