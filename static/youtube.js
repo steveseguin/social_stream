072b922c8947110d8ac542b1d3a6d75614935d10
steveseguin <punkrawker@gmail.com>
2023-06-20 19:52:54 -0400

    youtube static comments added

---
+++ static/youtube.js
@@ -0,0 +1,268 @@
+
+(function() {
+		
+	function toDataURL(url, callback) {
+	  var xhr = new XMLHttpRequest();
+	  xhr.onload = function() {
+		var reader = new FileReader();
+		reader.onloadend = function() {
+		  callback(reader.result);
+		}
+		reader.readAsDataURL(xhr.response);
+	  };
+	  xhr.open('GET', url);
+	  xhr.responseType = 'blob';
+	  xhr.send();
+	}
+	
+	var isExtensionOn = false;
+	
+	function pushMessage(data){
+		try{
+			chrome.runtime.sendMessage(chrome.runtime.id, { "message": data }, function(e){});
+		} catch(e){
+		}
+	}
+	
+	var settings = {};
+	// settings.textonlymode
+	// settings.captureevents
+	chrome.runtime.sendMessage(chrome.runtime.id, { "getSettings": true }, function(response){  // {"state":isExtensionOn,"streamID":channel, "settings":settings}
+		if ("settings" in response){
+			settings = response.settings;
+		}
+		if ("isExtensionOn" in response){
+			isExtensionOn = response.isExtensionOn;
+			
+			
+			if (document.getElementById("startupbutton")){
+				if (isExtensionOn){
+					document.getElementById("startupbutton").style.display = "block";
+				} else {
+					document.getElementById("startupbutton").style.display = "none";
+				}
+			}
+		}
+	});
+	
+	function getAllContentNodes(element) {
+		var resp = "";
+		
+		if (!element.childNodes.length || !element.childNodes){
+			return element.textContent || "";
+		}
+		
+		element.childNodes.forEach(node=>{
+			if (node.childNodes.length){
+				resp += getAllContentNodes(node)
+			} else if ((node.nodeType === 3) && (node.textContent.trim().length > 0)){
+				resp += node.textContent;
+			} else if (node.nodeType === 1){
+				resp += node.outerHTML;
+			}
+		});
+		return resp;
+	}
+	
+	chrome.runtime.onMessage.addListener(
+		function (request, sender, sendResponse) {
+			try {
+				// if ("focusChat" == request){ // if (prev.querySelector('[id^="message-username-"]')){ //slateTextArea-
+					// document.querySelector('textarea.comment-sender_input').focus();
+					// sendResponse(true);
+					// return;
+				// }
+				if (typeof request === "object"){
+					if ("settings" in request){
+						settings = request.settings;
+						
+					}
+					if ("isExtensionOn" in request){
+						isExtensionOn = request.isExtensionOn;
+						
+						if (document.getElementById("startupbutton")){
+							if (isExtensionOn){
+								document.getElementById("startupbutton").style.display = "block";
+							} else {
+								document.getElementById("startupbutton").style.display = "none";
+							}
+						}
+					}
+					sendResponse(true);
+					return;
+				}
+			} catch(e){}
+			sendResponse(false);
+		}
+	);
+	
+	function toDataURL(url, callback) {
+	  var xhr = new XMLHttpRequest();
+	  xhr.onload = function() {
+		var reader = new FileReader();
+		reader.onloadend = function() {
+		  callback(reader.result);
+		}
+		reader.readAsDataURL(xhr.response);
+	  };
+	  xhr.open('GET', url);
+	  xhr.responseType = 'blob';
+	  xhr.send();
+	}
+
+	function escapeHtml(unsafe){
+		return unsafe
+			 .replace(/&/g, "&amp;")
+			 .replace(/</g, "&lt;")
+			 .replace(/>/g, "&gt;")
+			 .replace(/"/g, "&quot;")
+			 .replace(/'/g, "&#039;");
+	 }
+
+	function prepMessage(){
+		
+	  var ele = this.targetEle;
+	  
+	  var chatname = "";
+	  try{
+			  chatname = ele.querySelector("#ytd-channel-name #text").innerText.trim();
+			  chatname = escapeHtml(chatname);
+	  } catch(e){
+			chatname = ele.querySelector("#header #author-text").innerText.trim();
+			chatname = chatname.replace("@", "");
+			chatname = escapeHtml(chatname);
+	  }
+	  
+	 
+	  
+	  var chatimg="";
+	  try{
+		 chatimg = ele.querySelector("#author-thumbnail #img[src]").src;
+	  } catch(e){
+		   console.error(e);
+	  }
+	  
+	  var chatmessage = "";
+	  try { 
+		  chatmessage = getAllContentNodes(ele.querySelector("#content #content-text"));
+	  } catch(e){
+		   console.error(e);
+		  return;
+	  }
+	  
+	  var chatdonation = false;
+	  var chatmembership = false;
+	  var chatsticker = false;
+	  
+	  this.style.backgroundColor = "#CCC!important";
+
+	  var data = {};
+	  data.chatname = chatname;
+	  data.chatbadges = "";
+	  data.backgroundColor = "";
+	  data.textColor = "";
+	  data.chatmessage = chatmessage;
+	  data.chatimg = chatimg;
+	  data.hasDonation = "";
+	  data.hasMembership = "";
+	  data.contentimg = "";
+	  data.type = "youtube";
+	  
+	  
+	  if (chatimg){
+		  toDataURL(data.chatimg, function(base64Image){ // we upscale
+				data.backupChatimg = data.chatimg.replace("=s32-", "=s256-");  // Increases the resolution of the image
+				data.backupChatimg = data.backupChatimg.replace("=s64-", "=s256-");
+				
+				if (base64Image){
+					data.chatimg = base64Image; // there's code in the index page to fallback if the larger image doens't exist
+				}
+				pushMessage(data);
+			});
+	  } else {
+		pushMessage(data);
+	  }
+	};
+	
+	function checkButtons(){
+		
+		if (!isExtensionOn){return;}
+		
+		var bases = document.querySelectorAll('#contents ytd-comment-thread-renderer');
+		for (var i=0;i<bases.length;i++) {
+			try {
+				if (!bases[i].dataset.set){
+					bases[i].dataset.set=true;
+					var button  = document.createElement("button");
+					button.onclick = prepMessage;
+					button.innerHTML = "Send to SocialStream";
+					button.style = "cursor:pointer;border: 0px; transition: all 0.2s linear 0s; border-radius: 100px; padding: 5px 10px; cursor: pointer; display: inline-block; border: 1.5px black solid; background-color: white;";
+					button.targetEle = bases[i]
+					//bases[i].appendChild(button);
+					try{
+						bases[i].querySelector('#toolbar').appendChild(button);
+					} catch(e){
+						
+					}
+				}
+			} catch(e){}
+		}
+	}
+	function startup() {
+		checkButtons();
+		setInterval(function(){
+			checkButtons();
+		}, 2000);
+	}
+
+	function preStartup(){
+		if (!document.getElementById("startupbutton")){
+			var button  = document.createElement("button");
+			button.onclick = function(){
+				document.getElementById("startupbutton").remove();
+				clearTimeout(preStartupInteval);
+				startup();
+			};
+			button.id = "startupbutton";
+			button.innerHTML = "SS";
+			button.title = "Enable Social Stream static comment capture support. Turn off the Social stream extension to hide this button";
+	
+			button.style = "margin-right: 10px; transition: all 0.2s linear 0s; border-radius: 100px; cursor: pointer; display: inline-block; background-color: white;";
+			
+			if (!isExtensionOn){
+				button.style.display = "none";
+			} else {
+				button.style.display = "inline-block";
+			}
+			
+			try{
+				document.querySelector('#container #buttons').prepend(button);
+			} catch (e){
+				
+			}
+		}
+	}
+
+	console.log("SOCIAL STREAM STATIC INJECTED");
+
+	setTimeout(function(){preStartup();},1000);
+
+	var preStartupInteval = setInterval(function(){preStartup();},5000);
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ youtube_comments.js
@@ -0,0 +1,376 @@
+(function () {
+	
+	function toDataURL(url, callback) {
+	  var xhr = new XMLHttpRequest();
+	  xhr.onload = function() {
+		var reader = new FileReader();
+		reader.onloadend = function() {
+		  callback(reader.result);
+		}
+		reader.readAsDataURL(xhr.response);
+	  };
+	  xhr.open('GET', url);
+	  xhr.responseType = 'blob';
+	  xhr.send();
+	}
+	
+	//var channelName = "";
+	
+	var messageHistory = [];
+	
+	function getAllContentNodes(element) {
+		var resp = "";
+		element.childNodes.forEach(node=>{
+			if (node.childNodes.length){
+				resp += getAllContentNodes(node)
+			} else if ((node.nodeType === 3) && (node.textContent.trim().length > 0)){
+				if (settings.textonlymode){
+					resp += node.textContent.trim()+" ";
+				} else {
+					resp += node.textContent.trim()+" ";
+				}
+			} else if (node.nodeType === 1){
+				if (settings.textonlymode){
+				//	if ("alt" in node){
+				//		resp += node.alt.trim()+" ";
+					//}
+				} else {
+					resp += node.outerHTML;
+				}
+			} 
+		});
+		return resp;
+	}
+	
+	function processMessage(ele, wss=true){
+		if (ele.hasAttribute("is-deleted")) {
+			//console.log("Message is deleted already");
+			return;
+		}
+
+		if (settings.customyoutubestate) {
+			return;
+		}
+		try {
+			if (ele.id && messageHistory.includes(ele.id)) {
+				//console.log("Message already exists");
+				return;
+			} else if (ele.id) {
+				messageHistory.push(ele.id);
+				messageHistory = messageHistory.slice(-200);
+			}
+			if (ele.querySelector("[in-banner]")) {
+				//console.log("Message in-banner");
+				return;
+			}
+		} catch (e) {}
+		//if (channelName && settings.customyoutubestate){
+		//if (settings.customyoutubeaccount && settings.customyoutubeaccount.textsetting && (settings.customyoutubeaccount.textsetting.toLowerCase() !== channelName.toLowerCase())){
+		//	return;
+		//} else if (!settings.customyoutubeaccount){
+		//	return;
+		//}
+		//  }
+
+		var chatmessage = "";
+		var chatname = "";
+		var chatimg = "";
+		var nameColor = "";
+		var memeber = false;
+		var mod = false;
+
+		var srcImg = ""; // what shows up as the source image; blank is default (dock decides).
+
+		try {
+			var nameElement = ele.querySelector("#author-name");
+			chatname = nameElement.innerText;
+
+			if (!settings.nosubcolor) {
+				if (nameElement.classList.contains("member")) {
+					nameColor = "#107516";
+					memeber = true;
+				} else if (nameElement.classList.contains("moderator")) {
+					nameColor = "#5f84f1";
+					mod = true;
+				}
+			}
+
+		} catch (e) {}
+
+		try {
+			var BTT = ele.querySelectorAll('.bttv-tooltip');
+			for (var i = 0; i < BTT.length; i++) {
+				BTT[i].outerHTML = "";
+			}
+		} catch (e) {}
+
+		if (!settings.textonlymode) {
+			try {
+				chatmessage = getAllContentNodes(ele.querySelector("#message, .seventv-yt-message-content"));
+			} catch (e) {}
+		} else {
+			try {
+				var cloned = ele.querySelector("#message, .seventv-yt-message-content").cloneNode(true);
+				//var children = cloned.querySelectorAll("[alt]");
+				//for (var i =0;i<children.length;i++){
+				//	children[i].outerHTML = children[i].alt;
+				//}
+				var children = cloned.querySelectorAll('[role="tooltip"]');
+				for (var i = 0; i < children.length; i++) {
+					children[i].outerHTML = "";
+				}
+				chatmessage = getAllContentNodes(cloned);
+			} catch (e) {}
+		}
+
+		try {
+			chatimg = ele.querySelector("#img").src;
+			if (chatimg.startsWith("data:image/gif;base64") || (ele.getAttribute("author-type") == "owner")) {
+				chatimg = document.querySelector("#panel-pages").querySelector("#img").src; // this is the owner
+			}
+		} catch (e) {}
+
+		var chatdonation = "";
+		try {
+			chatdonation = ele.querySelector("#purchase-amount").innerText;
+		} catch (e) {}
+
+		var chatmembership = "";
+		try {
+			chatmembership = ele.querySelector(".yt-live-chat-membership-item-renderer #header-subtext").innerHTML;
+		} catch (e) {}
+
+
+
+		var chatsticker = "";
+		try {
+			chatsticker = ele.querySelector(".yt-live-chat-paid-sticker-renderer #sticker>#img").src;
+		} catch (e) {}
+
+		if (chatsticker) {
+			chatdonation = ele.querySelector("#purchase-amount-chip").innerText;
+		}
+
+		var chatbadges = [];
+		try {
+			ele.querySelectorAll(".yt-live-chat-author-badge-renderer img, .yt-live-chat-author-badge-renderer svg").forEach(img => {
+				if (img.tagName.toLowerCase() == "img") {
+					var html = {};
+					html.src = img.src;
+					html.type = "img";
+					chatbadges.push(html);
+				} else if (img.tagName.toLowerCase() == "svg") {
+					var html = {};
+					img.style.fill = window.getComputedStyle(img).color;
+					html.html = img.outerHTML;
+					html.type = "svg";
+					chatbadges.push(html);
+				}
+			});
+
+		} catch (e) {}
+
+
+		var hasDonation = '';
+		if (chatdonation) {
+			hasDonation = chatdonation
+		}
+
+		var hasMembership = '';
+		var giftedmemembership = ele.querySelector("#primary-text.ytd-sponsorships-live-chat-header-renderer");
+
+		if (chatmembership) {
+			if (chatmessage) {
+				hasMembership = '<div class="donation membership">MEMBER CHAT</div>';
+			} else if (giftedmemembership) {
+				hasMembership = '<div class="donation membership">SPONSORSHIP</div>';
+				chatmessage = "<i>" + giftedmemembership.innerHTML + "</i>";
+			} else {
+				hasMembership = '<div class="donation membership">NEW MEMBER!</div>';
+				chatmessage = "<i>" + chatmembership + "</i>";
+			}
+		} else if (!chatmessage && giftedmemembership) {
+			chatmessage = "<i>" + giftedmemembership.innerHTML + "</i>";
+			hasMembership = '<div class="donation membership">SPONSORSHIP</div>';
+			// } else if (memeber){
+			//	  hasMembership = '<div class="membership">MEMEBER</div>'; // Just looks too green, and doesn't highlight those using special member options.
+			// } else if (mod){
+			//	  hasMembership = '<div class="membership">MODERATOR</div>';
+		}
+
+		if (chatsticker) {
+			chatmessage = '<img class="supersticker" src="' + chatsticker + '">';
+		}
+
+		var backgroundColor = "";
+
+		var textColor = "";
+		if (ele.style.getPropertyValue('--yt-live-chat-paid-message-primary-color')) {
+			backgroundColor = "background-color: " + ele.style.getPropertyValue('--yt-live-chat-paid-message-primary-color') + ";";
+			textColor = "color: #111;";
+		}
+
+		if (ele.style.getPropertyValue('--yt-live-chat-sponsor-color')) {
+			backgroundColor = "background-color: " + ele.style.getPropertyValue('--yt-live-chat-sponsor-color') + ";";
+			textColor = "color: #111;";
+		}
+
+		srcImg = document.querySelector("#input-panel");
+		if (srcImg) {
+			srcImg = srcImg.querySelector("#img");
+			if (srcImg) {
+				srcImg = srcImg.src || "";
+			} else {
+				srcImg = "";
+			}
+		} else {
+			srcImg = "";
+		}
+
+
+		if (!chatmessage && !hasDonation) {
+			//console.error("No message or donation");
+			return;
+		}
+
+		var data = {};
+		data.chatname = chatname;
+		data.nameColor = nameColor;
+		data.chatbadges = chatbadges;
+		data.backgroundColor = backgroundColor;
+		data.textColor = textColor;
+		data.chatmessage = chatmessage;
+		data.chatimg = chatimg;
+		data.hasDonation = hasDonation;
+		data.hasMembership = hasMembership;
+		data.type = "youtube";
+		try {
+			chrome.runtime.sendMessage(chrome.runtime.id, {
+				"message": data
+			}, function() {});
+		} catch(e){
+		}
+		
+	}
+	
+	chrome.runtime.onMessage.addListener(
+		function (request, sender, sendResponse) {
+			try{
+				if ("focusChat" == request){
+					document.querySelector("div#input").focus();
+					sendResponse(true);
+					return;
+				} 
+				if (typeof request === "object"){
+					if ("settings" in request){
+						settings = request.settings;
+						sendResponse(true);
+						return;
+					}
+				}
+				
+			} catch(e){}
+			sendResponse(false);
+		}
+	);
+	
+	var settings = {};
+	
+	chrome.runtime.sendMessage(chrome.runtime.id, { "getSettings": true }, function(response){  // {"state":isExtensionOn,"streamID":channel, "settings":settings}
+		if ("settings" in response){
+			settings = response.settings;
+		}
+	});
+
+	function onElementInserted(target, callback) {
+		var onMutationsObserved = function(mutations) {
+			mutations.forEach(function(mutation) {
+				if (mutation.addedNodes.length) {
+					for (var i = 0, len = mutation.addedNodes.length; i < len; i++) {
+						try{
+							if (mutation.addedNodes[i] && mutation.addedNodes[i].classList && mutation.addedNodes[i].classList.contains("yt-live-chat-banner-renderer")) {
+								continue;
+							} else if (mutation.addedNodes[i].tagName == "yt-live-chat-text-message-renderer".toUpperCase()) {
+								callback(mutation.addedNodes[i]);
+							} else if (mutation.addedNodes[i].tagName == "yt-live-chat-paid-message-renderer".toUpperCase()) {
+								callback(mutation.addedNodes[i]);
+							} else if (mutation.addedNodes[i].tagName == "yt-live-chat-membership-item-renderer".toUpperCase()) {
+								callback(mutation.addedNodes[i]);
+							} else if (mutation.addedNodes[i].tagName == "yt-live-chat-paid-sticker-renderer".toUpperCase()) {
+								callback(mutation.addedNodes[i]);
+							} else if (mutation.addedNodes[i].tagName == "ytd-sponsorships-live-chat-gift-purchase-announcement-renderer".toUpperCase()) {
+								callback(mutation.addedNodes[i]);
+							} else {
+								//console.error("unknown: "+mutation.addedNodes[i].tagName);
+							}
+						} catch(e){}
+					}
+				}
+			});
+		};
+		if (!target){return;}
+		var config = { childList: true, subtree: true };
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		var observer = new MutationObserver(onMutationsObserved);
+		observer.observe(target, config);
+	}
+
+    console.log("Social stream inserted");
+	
+    var ele = document.querySelector("yt-live-chat-app");
+	if (ele){
+		onElementInserted(ele, function(ele2){
+		    setTimeout(function(ele2){processMessage(ele2, false)}, 200, ele2);
+		});
+	}
+	
+	if (window.location.href.includes("youtube.com/watch")){
+		setTimeout(function(){
+			var ele = document.querySelector('iframe').contentWindow.document.body.querySelector("#chat-messages");
+			if (ele){
+				onElementInserted(ele, function(ele2){
+				     setTimeout(function(ele2){processMessage(ele2, false)}, 200, ele2);
+				});
+			}
+		},3000);
+	}
+	
+	///////// the following is a loopback webrtc trick to get chrome to not throttle this twitch tab when not visible.
+	try {
+		var receiveChannelCallback = function(e){
+			remoteConnection.datachannel = event.channel;
+			remoteConnection.datachannel.onmessage = function(e){};;
+			remoteConnection.datachannel.onopen = function(e){};;
+			remoteConnection.datachannel.onclose = function(e){};;
+			setInterval(function(){
+				if (document.hidden){ // only poke ourselves if tab is hidden, to reduce cpu a tiny bit.
+					remoteConnection.datachannel.send("KEEPALIVE")
+				}
+			}, 800);
+		}
+		var errorHandle = function(e){}
+		var localConnection = new RTCPeerConnection();
+		var remoteConnection = new RTCPeerConnection();
+		localConnection.onicecandidate = (e) => !e.candidate ||	remoteConnection.addIceCandidate(e.candidate).catch(errorHandle);
+		remoteConnection.onicecandidate = (e) => !e.candidate || localConnection.addIceCandidate(e.candidate).catch(errorHandle);
+		remoteConnection.ondatachannel = receiveChannelCallback;
+		localConnection.sendChannel = localConnection.createDataChannel("sendChannel");
+		localConnection.sendChannel.onopen = function(e){localConnection.sendChannel.send("CONNECTED");};
+		localConnection.sendChannel.onclose =  function(e){};
+		localConnection.sendChannel.onmessage = function(e){};
+		localConnection.createOffer()
+			.then((offer) => localConnection.setLocalDescription(offer))
+			.then(() => remoteConnection.setRemoteDescription(localConnection.localDescription))
+			.then(() => remoteConnection.createAnswer())
+			.then((answer) => remoteConnection.setLocalDescription(answer))
+			.then(() =>	{
+				localConnection.setRemoteDescription(remoteConnection.localDescription);
+				console.log("KEEP ALIVE TRICk ENABLED");
+			})
+			.catch(errorHandle);
+	} catch(e){
+		console.log(e);
+	}
+	
+	
+})();
