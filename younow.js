ec9f1cad62a8e55906b93c5b27cabda93342069a
steveseguin <punkrawker@gmail.com>
2023-12-08 03:29:37 -0500

    younow fixes and improvements

--- younow.js
+++ younow.js
@@ -53,6 +53,46 @@
 		return resp;
 	}
 	
+	function rankToColor(rank, maxRank = 400) {
+	  // Start and end colors in RGB
+	  const startColor = { r: 197, g: 204, b: 218 }; // #4F6692
+	  const midColor = { r: 100, g: 115, b: 225 };    // #2026B0
+	  const endColor = { r: 81, g: 85, b: 255 };      // #0000FF
+
+	  // Determine color stops based on rank
+	  const midRank = parseInt(maxRank/2);
+	  let colorStop;
+	  
+
+	  if (rank <= midRank) {
+		// Calculate how far the rank is between 1 and midRank
+		const ratio = (rank - 1) / (midRank - 1);
+		colorStop = {
+		  r: startColor.r + ratio * (midColor.r - startColor.r),
+		  g: startColor.g + ratio * (midColor.g - startColor.g),
+		  b: startColor.b + ratio * (midColor.b - startColor.b),
+		};
+	  } else {
+		// Calculate how far the rank is between midRank and maxRank
+		const ratio = (rank - midRank) / (maxRank - midRank);
+		colorStop = {
+		  r: midColor.r + ratio * (endColor.r - midColor.r),
+		  g: midColor.g + ratio * (endColor.g - midColor.g),
+		  b: midColor.b + ratio * (endColor.b - midColor.b),
+		};
+	  }
+
+	  // Convert the RGB color stop to a hex color code
+	  const hexColor = `#${Math.round(colorStop.r).toString(16).padStart(2, '0')}` +
+					   `${Math.round(colorStop.g).toString(16).padStart(2, '0')}` +
+					   `${Math.round(colorStop.b).toString(16).padStart(2, '0')}`;
+	  return hexColor;
+	}
+	var lut = [];
+	for (var i =1;i<=400;i++){
+		lut.push(rankToColor(i,400));
+	}
+	
 	
 	function processMessage(ele){
 		
@@ -67,7 +107,7 @@
 		
 		var name="";
 		try {
-			name = escapeHtml(ele.querySelector("button>span[title]").textContent.trim());
+			name = escapeHtml(ele.querySelector("button span[title]").textContent.trim());
 		} catch(e){
 		}
 
@@ -82,12 +122,34 @@
 			return;
 		}
 		
+		var chatbadges = [];
+		try {
+		  ele.querySelectorAll("user-badges img[src]").forEach(badge=>{
+			 if (badge.src){
+				chatbadges.push(badge.src);
+			 }
+		  });
+		} catch(e){}
+		
+		
+		
+		var nameColor = "";
+		
+		try {
+			var rank = parseInt(ele.querySelector(".user-level").innerText) || 1;
+			if (!settings.nosubcolor) {
+				if (rank>400){rank=400;}
+				nameColor = lut[rank];
+			}
+		} catch(e){
+		}
+		
 		var data = {};
 		data.chatname = name;
-		data.chatbadges = "";
+		data.chatbadges = chatbadges;
 		data.backgroundColor = "";
 		data.textColor = "";
-		data.nameColor = "";
+		data.nameColor = nameColor
 		data.chatmessage = msg;
 		data.chatimg = chatimg;
 		data.hasDonation = "";
