3930784c18ec9870782cdb1c3a50a9448fa76e59
steveseguin <punkrawker@gmail.com>
2024-02-13 09:28:55 -0500

    further twitchwss fixes

--- wss/twitch.js
+++ wss/twitch.js
@@ -1,149 +1,186 @@
-	var clientId = 'sjjsgy1sgzxmy346tdkghbyz4gtx0k'; 
-	var redirectURI = 'https://socialstream.ninja/wss/twitch.html';
-	var scope = 'chat%3Aread+chat%3Aedit';
-	var ws;
-	var token = "";
+var clientId = 'sjjsgy1sgzxmy346tdkghbyz4gtx0k'; 
+var redirectURI = 'https://socialstream.ninja/wss/twitch.html';
+var scope = 'chat%3Aread+chat%3Aedit';
+var ws;
+var token = "";
 
-	function parseFragment(hash) {
-		var hashMatch = function(expr) {
-		  var match = hash.match(expr);
-		  return match ? match[1] : null;
+function parseFragment(hash) {
+	var hashMatch = function(expr) {
+	  var match = hash.match(expr);
+	  return match ? match[1] : null;
+	};
+	var state = hashMatch(/state=(\w+)/);
+	token = hashMatch(/access_token=(\w+)/);
+	if (sessionStorage.twitchOAuthState == state)
+		sessionStorage.twitchOAuthToken = hashMatch(/access_token=(\w+)/);
+	return
+};
+
+function authUrl() {
+	sessionStorage.twitchOAuthState = nonce(15);
+	var url = 'https://id.twitch.tv/oauth2/authorize' +
+		'?response_type=token' +
+		'&client_id=' + clientId + 
+		'&redirect_uri=' + redirectURI +
+		'&state=' + sessionStorage.twitchOAuthState +
+		'&scope=' + scope;
+	return url
+}
+
+// Source: https://www.thepolyglotdeveloper.com/2015/03/create-a-random-nonce-string-using-javascript/
+function nonce(length) {
+	var text = "";
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	for (var i = 0; i < length; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+
+const wsUri = 'wss://irc-ws.chat.twitch.tv:443';
+
+let websocket;
+
+function connect() {
+  websocket = new WebSocket(wsUri);
+
+  websocket.onopen = () => {
+	console.log('Connected');
+	// Authenticate and join a channel
+	
+	websocket.send(`PASS oauth:${sessionStorage.twitchOAuthToken || token}`);
+	websocket.send(`NICK ${username}`); // 
+	websocket.send(`JOIN #${channel}`);
+  };
+
+  websocket.onmessage = (event) => {
+	const messages = event.data.split(/\r?\n/);
+	messages.forEach((rawMessage) => {
+		if (rawMessage) {
+			const parsedMessage = parseMessage(rawMessage);
+			
+			if (parsedMessage.command === 'PING') {
+				websocket.send('PONG :tmi.twitch.tv');
+			} else if (parsedMessage.command === 'PRIVMSG') {
+				processMessage(parsedMessage);
+			} else {
+				console.log(rawMessage);
+			}
+		}
+	  });
+	};
+		
+
+  websocket.onerror = (event) => {
+	console.error('WebSocket error:', event);
+  };
+
+  websocket.onclose = (event) => {
+	console.log('Disconnected:', event);
+	console.log('Attempting to reconnect...');
+	setTimeout(connect, 10000); // Reconnect after 10 seconds
+  };
+}
+
+function parseMessage(rawMessage) {
+  const parsedMessage = {
+	prefix: null,
+	command: null,
+	params: [],
+	trailing: null
+  };
+
+  // Split the message into prefix, command/params, and trailing parts
+  const match = rawMessage.match(/^:([^ ]+) +([^ :]+)(?: +([^ :]+))*(?: +:(.*))?$/);
+  
+  if (match) {
+	parsedMessage.prefix = match[1];
+	parsedMessage.command = match[2];
+	if (match[3]) {
+	  parsedMessage.params = match[3].split(' ');
+	} 
+	if (match[4]){
+		parsedMessage.trailing = match[4];
+	}
+  } else if (rawMessage == "PING :tmi.twitch.tv"){
+	 parsedMessage.command = "PING";
+  } else {
+	// If the regex does not match, the message might be a simple command or malformed.
+	console.error('Failed to parse message:', rawMessage);
+  }
+
+  return parsedMessage;
+}
+
+
+function sendMessage(message) {
+	if (websocket.readyState === WebSocket.OPEN) {
+		websocket.send(`PRIVMSG #${channel} :${message}`);
+	} else {
+		console.error('WebSocket is not open.');
+	}
+}
+
+
+(function (w) {
+	w.URLSearchParams = w.URLSearchParams || function (searchString) {
+		var self = this;
+		self.searchString = searchString;
+		self.get = function (name) {
+			var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(self.searchString);
+			if (results == null) {
+				return null;
+			} else {
+				return decodeURI(results[1]) || 0;
+			}
 		};
-		var state = hashMatch(/state=(\w+)/);
-		token = hashMatch(/access_token=(\w+)/);
-		if (sessionStorage.twitchOAuthState == state)
-			sessionStorage.twitchOAuthToken = hashMatch(/access_token=(\w+)/);
-		return
 	};
 
-	function authUrl() {
-		sessionStorage.twitchOAuthState = nonce(15);
-		var url = 'https://id.twitch.tv/oauth2/authorize' +
-			'?response_type=token' +
-			'&client_id=' + clientId + 
-			'&redirect_uri=' + redirectURI +
-			'&state=' + sessionStorage.twitchOAuthState +
-			'&scope=' + scope;
-		return url
-	}
+})(window);
 
-	// Source: https://www.thepolyglotdeveloper.com/2015/03/create-a-random-nonce-string-using-javascript/
-	function nonce(length) {
-		var text = "";
-		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		for (var i = 0; i < length; i++) {
-			text += possible.charAt(Math.floor(Math.random() * possible.length));
+var urlParams = new URLSearchParams(window.location.search);
+
+const username = "SocialStreamNinja"; // Not supported at the moment
+var channel = urlParams.get("channel") || urlParams.get("username") || '';
+
+
+
+
+if (document.location.hash.match(/access_token=(\w+)/)){
+	parseFragment(document.location.hash);
+}
+if (sessionStorage.twitchOAuthToken || token) {
+	
+	if (!channel){
+		channel = prompt("What is the channel you wish to join?");
+		if (!channel){
+			channel = 'vdoninja';
 		}
-		return text;
+		document.location.href += "&channel="+channel;
 	}
 
+	console.log("channel: "+channel);
+	getTwitchAvatarImage(channel);
+
+	connect();
+	document.querySelector('.socket').classList.remove("hidden");
 	
-	const username = "SocialStreamNinja"; // Not supported at the moment
-	const channel = 'vdoninja'; // Replace with the channel you want to join
-
-	const wsUri = 'wss://irc-ws.chat.twitch.tv:443';
-
-	let websocket;
-
-	function connect() {
-	  websocket = new WebSocket(wsUri);
-
-	  websocket.onopen = () => {
-		console.log('Connected');
-		// Authenticate and join a channel
-		websocket.send(`PASS oauth:${sessionStorage.twitchOAuthToken || token}`);
-		websocket.send(`NICK ${username}`); // 
-		websocket.send(`JOIN #${channel}`);
-	  };
-
-	  websocket.onmessage = (event) => {
-		const messages = event.data.split(/\r?\n/);
-		messages.forEach((rawMessage) => {
-			if (rawMessage) {
-				const parsedMessage = parseMessage(rawMessage);
-				
-				if (parsedMessage.command === 'PING') {
-					websocket.send('PONG :tmi.twitch.tv');
-				} else if (parsedMessage.command === 'PRIVMSG') {
-					processMessage(parsedMessage);
-				} else {
-					console.log(rawMessage);
-				}
-			}
-		  });
-		};
-			
-
-	  websocket.onerror = (event) => {
-		console.error('WebSocket error:', event);
-	  };
-
-	  websocket.onclose = (event) => {
-		console.log('Disconnected:', event);
-		console.log('Attempting to reconnect...');
-		setTimeout(connect, 10000); // Reconnect after 10 seconds
-	  };
-	}
+} else {
+	var url = authUrl()
+	document.querySelector('#auth-link').href = url;
+	document.querySelector('.auth').classList.remove("hidden");
 	
-	function parseMessage(rawMessage) {
-	  const parsedMessage = {
-		prefix: null,
-		command: null,
-		params: [],
-		trailing: null
-	  };
-
-	  // Split the message into prefix, command/params, and trailing parts
-	  const match = rawMessage.match(/^:([^ ]+) +([^ :]+)(?: +([^ :]+))*(?: +:(.*))?$/);
-	  
-	  if (match) {
-		parsedMessage.prefix = match[1];
-		parsedMessage.command = match[2];
-		if (match[3]) {
-		  parsedMessage.params = match[3].split(' ');
-		} 
-		if (match[4]){
-			parsedMessage.trailing = match[4];
-		}
-	  } else if (rawMessage == "PING :tmi.twitch.tv"){
-		 parsedMessage.command = "PING";
-	  } else {
-		// If the regex does not match, the message might be a simple command or malformed.
-		console.error('Failed to parse message:', rawMessage);
-	  }
-
-	  return parsedMessage;
-	}
-
 	
-	function sendMessage(message) {
-		if (websocket.readyState === WebSocket.OPEN) {
-			websocket.send(`PRIVMSG #${channel} :${message}`);
-		} else {
-			console.error('WebSocket is not open.');
-		}
-	}
+}
 
-	
-	if (document.location.hash.match(/access_token=(\w+)/))
-		parseFragment(document.location.hash);
-	if (sessionStorage.twitchOAuthToken || token) {
-		connect();
-		document.querySelector('.socket').classList.remove("hidden");
-		
-	} else {
-		var url = authUrl()
-		document.querySelector('#auth-link').href = url;
-		document.querySelector('.auth').classList.remove("hidden");
+document.querySelector('button').onclick = function(){
+	var msg = document.querySelector('#input-text').value.trim();;
+	if (msg){
+		sendMessage(msg);
+		document.querySelector('#input-text').value = "";
 	}
-	
-	document.querySelector('button').onclick = function(){
-		var msg = document.querySelector('#input-text').value.trim();;
-		if (msg){
-			sendMessage(msg);
-			document.querySelector('#input-text').value = "";
-		}
-	}
+}
 	
 
 	////////
@@ -203,18 +240,23 @@ function getTwitchAvatarImage(username) {
 	});
 }
 
-getTwitchAvatarImage(channel);
 
 
 function processMessage(parsedMessage){
-	
-	const channel = parsedMessage.params[0];
+	console.log(parsedMessage);
+	var chan = parsedMessage.params[0];
+	//
+	console.log("channel: "+chan);
 	const message = parsedMessage.trailing;
 	const user = parsedMessage.prefix.split('!')[0];
 	
-	console.log(`Message from ${user} in ${channel}: ${message}`);
-	document.querySelector("textarea").innerHTML += user+": "+message+"\n";
-	document.querySelector("textarea").value = document.querySelector("textarea").value.substring(document.querySelector("textarea").value.length - 500);
+	console.log(`Message from ${user} in ${chan}: ${message}`);
+	var span = document.createElement("div");
+	span.innerText += user+": "+message;
+	document.querySelector("#textarea").appendChild(span);
+	if (document.querySelector("#textarea").childNodes.length>10){
+		document.querySelector("#textarea").childNodes[0].remove();
+	}
 	
 	var data = {};
 	data.chatname = user;
