6d57d28f0ba55c3b90748aebaad9e633d1bec178
steveseguin <punkrawker@gmail.com>
2024-05-23 14:05:30 -0400

    zap.stream added

---
+++ sources/zapstream.js
@@ -0,0 +1,197 @@
+(function () {
+	 
+	function toDataURL(url, callback) {
+	  var xhr = new XMLHttpRequest();
+	  xhr.onload = function() {
+		  
+		var blob = xhr.response;
+    
+		if (blob.size > (25 * 1024)) {
+		  callback(url); // Image size is larger than 25kb.
+		  return;
+		}
+
+		var reader = new FileReader();
+		
+		
+		reader.onloadend = function() {
+		  callback(reader.result);
+		}
+		reader.readAsDataURL(xhr.response);
+	  };
+	  xhr.open('GET', url);
+	  xhr.responseType = 'blob';
+	  xhr.send();
+	}
+
+	function escapeHtml(unsafe){ // success is when goofs be trying to hack me
+		return unsafe
+			 .replace(/&/g, "&amp;")
+			 .replace(/</g, "&lt;")
+			 .replace(/>/g, "&gt;")
+			 .replace(/"/g, "&quot;")
+			 .replace(/'/g, "&#039;") || "";
+	}
+	function getAllContentNodes(element) {
+		var resp = "";
+		
+		if (!element.childNodes || !element.childNodes.length){
+			if (element.nodeType===3){
+				return escapeHtml(element.textContent) || "";
+			}
+		}
+		
+		element.childNodes.forEach(node=>{
+			if (node.childNodes.length){
+				resp += getAllContentNodes(node)
+			} else if ((node.nodeType === 3) && node.textContent && (node.textContent.trim().length > 0)){
+				resp += escapeHtml(node.textContent);
+			} else if (node.nodeType === 1){
+				if (!settings.textonlymode){
+					if ((node.nodeName == "IMG") && node.src){
+						node.src = node.src+"";
+					}
+					resp += node.outerHTML;
+				}
+			}
+		});
+		return resp;
+	}
+	
+	
+	function processMessage(ele){
+		
+		var chatimg = "";
+		try{
+		   chatimg = ele.querySelector(":scope > a > img[src]").src;
+		} catch(e){
+		//	console.warn(e);
+		}
+		
+		var name="";
+		try {
+			name = ele.querySelector(":scope > a > span").textContent.trim();
+			name = escapeHtml(name);
+		} catch(e){
+		//	console.warn(e);
+		}
+		var msg="";
+		try {
+			ele.querySelectorAll(":scope > span[title] > span").forEach(xx=>{
+				msg+= getAllContentNodes(xx);
+			});
+		} catch(e){
+		//	console.warn(e);
+		}
+		
+		if (!name || !msg){
+			return;
+		}
+		
+		var data = {};
+		data.chatname = name;
+		data.chatbadges = "";
+		data.backgroundColor = "";
+		data.textColor = "";
+		data.chatmessage = msg;
+		data.chatimg = chatimg;
+		data.hasDonation = "";
+		data.membership = "";;
+		data.contentimg = "";
+		data.textonly = settings.textonlymode || false;
+		data.type = "zapstream";
+		
+		console.log(data);
+		
+		pushMessage(data);
+	}
+
+	function pushMessage(data){
+		try{
+			chrome.runtime.sendMessage(chrome.runtime.id, { "message": data }, function(e){});
+		} catch(e){
+		}
+	}
+	
+	var settings = {};
+	// settings.textonlymode
+	// settings.captureevents
+	
+	
+	chrome.runtime.sendMessage(chrome.runtime.id, { "getSettings": true }, function(response){  // {"state":isExtensionOn,"streamID":channel, "settings":settings}
+		if ("settings" in response){
+			settings = response.settings;
+		}
+	});
+
+	chrome.runtime.onMessage.addListener(
+		function (request, sender, sendResponse) {
+			try{
+				if ("focusChat" == request){ // if (prev.querySelector('[id^="message-username-"]')){ //slateTextArea-
+					document.querySelector('textarea').focus();
+					sendResponse(true);
+					return;
+				}
+				if (typeof request === "object"){
+					if ("settings" in request){
+						settings = request.settings;
+						sendResponse(true);
+						return;
+					}
+				}
+			} catch(e){}
+			sendResponse(false);
+		}
+	);
+
+	var lastURL =  "";
+	var observer = null;
+	
+	
+	function onElementInserted(containerSelector) {
+		var target = document.querySelector(containerSelector);
+		if (!target){return;}
+		
+		
+		var onMutationsObserved = function(mutations) {
+			mutations.forEach(function(mutation) {
+				if (mutation.addedNodes.length) {
+					
+					for (var i = 0, len = mutation.addedNodes.length; i < len; i++) {
+						try {
+							if (mutation.addedNodes[i].skip){continue;}
+							mutation.addedNodes[i].skip = true;
+							setTimeout(function(ele){
+								processMessage(ele);
+							},500,mutation.addedNodes[i])
+						} catch(e){}
+					}
+				}
+			});
+		};
+		
+		var config = { childList: true, subtree: false };
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		
+		observer = new MutationObserver(onMutationsObserved);
+		observer.observe(target, config);
+	}
+	
+	console.log("social stream injected");
+
+	setInterval(function(){
+		try {
+		if (document.querySelector('.flex.flex-col-reverse')){
+			if (!document.querySelector('.flex.flex-col-reverse').marked){
+				document.querySelector('.flex.flex-col-reverse').marked=true;
+				console.log("CONNECTED chat detected");
+				[...document.querySelectorAll(".flex.flex-col-reverse > div")].reverse().forEach(ele=>{
+						ele.skip=true;
+						//processMessage(ele);
+					});
+					onElementInserted('.flex.flex-col-reverse');
+			}
+		}} catch(e){}
+	},2000);
+
+})();
