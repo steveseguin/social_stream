b46ecf8ffe877b6f1ef10fcc4dbff972065ca33c
steveseguin <punkrawker@gmail.com>
2024-08-19 22:49:28 -0400

    facebook tweak again

--- sources/facebook.js
+++ sources/facebook.js
@@ -382,7 +382,7 @@
 			processed += 1;
 		}
 		try {
-			if (window.location.href.includes("/live/producer/") || window.location.href.includes("/videos")) {
+			if (window.location.href.includes("/live/producer/") || window.location.href.endsWith("/videos") || window.location.href.includes("/videos/") || window.location.href.includes("?v=")) {
 				var main = document.querySelectorAll("[role='article']");
 				for (var j = 0; j < main.length; j++) {
 					try {
