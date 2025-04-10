7676e8d6880085cc531d8899dc9373a6ddb4a63a
steveseguin <punkrawker@gmail.com>
2025-04-09 22:06:36 -0400

    arena.social

---
+++ sources/arenasocial.js
@@ -0,0 +1,259 @@
+(function () {
+	 
+	function toDataURL(url, callback) {
+	  var xhr = new XMLHttpRequest();
+	  xhr.onload = function() {
+		  
+		var blob = xhr.response;
+    
+		if (blob.size > (55 * 1024)) {
+		  callback(url); // Image size is larger than 25kb.
+		  return;
+		}
+
+		var reader = new FileReader();
+		
+		
+		reader.onloadend = function() {
+		  callback(reader.result);
+		}
+		reader.readAsDataURL(xhr.response);
+	  };
+	  xhr.open('GET', url);
+	  xhr.responseType = 'blob';
+	  xhr.send();
+	}
+
+	function escapeHtml(unsafe){ // when goofs be trying to hack me
+		return unsafe
+			 .replace(/&/g, "&amp;")
+			 .replace(/</g, "&lt;")
+			 .replace(/>/g, "&gt;")
+			 .replace(/"/g, "&quot;")
+			 .replace(/'/g, "&#039;") || "";
+	}
+
+	function getAllContentNodes(element) { // takes an element.
+		var resp = "";
+		
+		if (!element){return resp;}
+		
+		if (!element.childNodes || !element.childNodes.length){
+			if (element.textContent){
+				return escapeHtml(element.textContent) || "";
+			} else {
+				return "";
+			}
+		}
+		
+		element.childNodes.forEach(node=>{
+			if (node.childNodes.length){
+				resp += getAllContentNodes(node)
+			} else if ((node.nodeType === 3) && node.textContent && (node.textContent.trim().length > 0)){
+				resp += escapeHtml(node.textContent)+" ";
+			} else if (node.nodeType === 1){
+				if (!settings.textonlymode){
+					if ((node.nodeName == "IMG") && node.src){
+						node.src = node.src+"";
+					}
+					resp += node.outerHTML;
+				}
+			}
+		});
+		return resp;
+	}
+	
+	var settings = {};
+	// settings.textonlymode
+	// settings.captureevents
+	
+	var channelName = "";
+	
+	function processMessage(ele){
+		
+		var chatimg = ""
+
+		try {
+			chatimg = ele.querySelector("img.aspect-square[src]").src;
+		} catch(e){
+		}
+		
+		var name="";
+		try {
+			name = escapeHtml(ele.querySelector(".flex-grow.text-sm > [class][style][role]").textContent);
+		} catch(e){
+		}
+		
+		var namecolor="";
+		try {
+			namecolor = ele.querySelector(".flex-grow.text-sm > [class][style][role]").style.color;
+		} catch(e){
+		}
+		
+		var badges=[];
+		/* try {
+			ele.querySelectorAll("img[class^='ChatBadge_image_'][src]").forEach(badge=>{
+				badges.push(badge.src);
+			});
+		} catch(e){
+		} */
+
+		var msg="";
+		try {
+			msg = getAllContentNodes(ele.querySelector(".flex-grow.text-sm > div.inline > span")).trim();
+		} catch(e){
+		}
+		
+		
+		if (!msg || !name){
+			return;
+		}
+		
+		
+		var data = {};
+		data.chatname = name;
+		data.chatbadges = badges;
+		data.backgroundColor = "";
+		data.textColor = "";
+		data.nameColor = namecolor;
+		data.chatmessage = msg;
+		data.chatimg = chatimg;
+		data.hasDonation = "";
+		data.membership = "";
+		data.contentimg = "";
+		data.textonly = settings.textonlymode || false;
+		data.type = "arenasocial";
+		
+		
+		pushMessage(data);
+	}
+
+	function pushMessage(data){
+		try{
+			chrome.runtime.sendMessage(chrome.runtime.id, { "message": data }, function(e){});
+		} catch(e){
+		}
+	}
+	var isExtensionOn = true;
+	
+	function checkViewers(){
+		if (isExtensionOn && (settings.showviewercount || settings.hypemode)){
+			try {
+				let viewerSpan = document.querySelector("svg > path[d='M15 12a3 3 0 11-6 0 3 3 0 016 0z']").parentNode.nextElementSibling;
+				if (viewerSpan && viewerSpan.textContent){
+					let views = viewerSpan.textContent.toUpperCase();
+					let multiplier = 1;
+					if (views.includes("K")){
+						multiplier = 1000;
+						views = views.replace("K","");
+					} else if (views.includes("M")){
+						multiplier = 1000000;
+						views = views.replace("M","");
+					}
+					views = views.split(" ")[0];
+					if (views == parseFloat(views)){
+						views = parseFloat(views) * multiplier;
+						chrome.runtime.sendMessage(
+							chrome.runtime.id,
+							({message:{
+									type: 'arenasocial',
+									event: 'viewer_update',
+									meta: views
+								}
+							}),
+							function (e) {}
+						);
+					}
+				}
+			} catch (e) {
+			}
+		}
+	}
+
+
+	// OnlineViewers_root_orkvv
+	
+	chrome.runtime.sendMessage(chrome.runtime.id, { "getSettings": true }, function(response){  // {"state":isExtensionOn,"streamID":channel, "settings":settings}
+		if ("settings" in response){
+			settings = response.settings;
+		}
+		if ("state" in response){
+			isExtensionOn = response.state;
+		}
+	});
+
+	chrome.runtime.onMessage.addListener(
+		function (request, sender, sendResponse) {
+			try{
+				if ("focusChat" == request){ // if (prev.querySelector('[id^="message-username-"]')){ //slateTextArea-
+				
+					document.querySelector('#type-a-message').focus();
+					sendResponse(true);
+					return;
+				}
+				if (typeof request === "object"){
+					if ("state" in request) {
+						isExtensionOn = request.state;
+					}
+					if ("settings" in request){
+						settings = request.settings;
+						sendResponse(true);
+						return;
+					}
+				}
+				
+			} catch(e){}
+			sendResponse(false);
+		}
+	);
+
+	var lastURL =  "";
+	var observer = null;
+	
+	
+	function onElementInserted(target) {
+		var onMutationsObserved = function(mutations) {
+			mutations.forEach(function(mutation) {
+				if (mutation.addedNodes.length) {
+					for (var i = 0, len = mutation.addedNodes.length; i < len; i++) {
+						try {
+							if (mutation.addedNodes[i].skip){continue;}
+
+							mutation.addedNodes[i].skip = true;
+
+							processMessage(mutation.addedNodes[i]); // maybe here
+							
+						} catch(e){}
+					}
+				}
+			});
+		};
+		
+		var config = { childList: true, subtree: false };
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		
+		observer = new MutationObserver(onMutationsObserved);
+		observer.observe(target, config);
+	}
+	
+	console.log("social stream injected");
+
+
+
+	setInterval(function(){
+		try {
+			var container = document.querySelector("[data-testid='virtuoso-item-list']");
+			if (!container.marked){
+				container.marked=true;
+
+				console.log("CONNECTED chat detected");
+
+				setTimeout(function(){
+					onElementInserted(container);
+				},2000);
+			}
+			checkViewers();
+		} catch(e){}
+	},2000);
+
+})();
