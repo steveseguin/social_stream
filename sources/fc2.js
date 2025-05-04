c3b33dd1590d9b5c9986099f80871f8527cb9caa
steveseguin <punkrawker@gmail.com>
2025-05-04 18:53:39 -0400

    fc2 fix for image

--- sources/fc2.js
+++ sources/fc2.js
@@ -99,6 +99,15 @@
 			return;
 		}
 		
+		var chatimg = "";
+		try {
+			chatimg = ele.querySelector('img[src]').src;
+			if (chatimg == "https://live.fc2.com/common/live/img/thumb_profile_small.png"){
+				chatimg = "";
+			}
+		} catch(e){
+		}
+		
 		//data.sourceImg = brandedImageURL;
 		
 		var sourceImg = "fc2.png";
@@ -109,7 +118,7 @@
 		data.textColor = "";
 		data.nameColor = nameColor;
 		data.chatmessage = msg;
-		data.chatimg = "";
+		data.chatimg = chatimg;
 		data.hasDonation = "";
 		data.membership = "";
 		data.contentimg = "";
